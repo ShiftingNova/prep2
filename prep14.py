d6f4d2c1f8e50fa1ba58e94babb94e6439bd7be1
ShiftingNova <89542928+ShiftingNova@users.noreply.github.com>
2021-09-29 13:18:35 -0700

    Update prep14.py

--- prep14.py
+++ prep14.py
@@ -4,4 +4,5 @@ def calculate_tree_height(height, year):
     while i<year:
         total = total * 1.2
         i+=1
-    print(round(total,4),end="")
+        total = round(total,4)
+    print(total)
